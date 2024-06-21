187856db6c1e125a26d85c7384863e414852eafb
Pascal Schreiber <info@pascalschreiber.de>
2024-06-21 12:43:11 +0200

    added config_schema

--- custom_components/meshcentral/__init__.py
+++ custom_components/meshcentral/__init__.py
@@ -4,10 +4,20 @@ from homeassistant.core import HomeAssistant
 from .meshcentral_websocket import connect_websocket
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_URL
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 DOMAIN = "meshcentral"
 PLATFORMS = ["binary_sensor", "sensor"]
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Required('url', default='localhost:5222'): cv.string,
+        vol.Required('username', default='admin'): cv.string,
+        vol.Required('password'): cv.string,
+    })
+}, extra=vol.ALLOW_EXTRA)
 
 async def async_setup(hass: HomeAssistant, config):
 
