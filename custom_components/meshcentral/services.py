8fd59c19e8479ee91d42337e357c13a6321d5ff0
Pascal Schreiber <info@pascalschreiber.de>
2025-02-01 13:30:04 +0100

    Added Services

--- custom_components/meshcentral/services.py
+++ custom_components/meshcentral/services.py
@@ -3,6 +3,9 @@ from homeassistant.core import HomeAssistant, ServiceCall
 import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 from .const import DOMAIN
+from homeassistant.helpers.device_registry import async_get as async_get_device
+from homeassistant.helpers.entity_registry import async_get as async_get_entity
+from homeassistant.helpers.entity_registry import async_entries_for_device
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -10,10 +13,25 @@ async def setup_services(hass: HomeAssistant, websocket, send_command):
     _LOGGER.info("Setting up Services")
 
     async def handle_power(call: ServiceCall):
-        entity_id = call.data.get("entity_id")
+        device = call.data.get("device")
         mode = call.data.get("mode")
 
-        entity = hass.states.get(entity_id)
+        device_registry = async_get_device(hass)
+        entity_registry = async_get_entity(hass)
+
+        # Lade Device
+        device_entry = device_registry.async_get(device)
+
+        if device_entry:
+            entities = async_entries_for_device(entity_registry, device_entry.id)
+            
+            if entities:
+                entity = hass.states.get(entities[0].entity_id)
+            else:
+                _LOGGER.error(f"No entities found for device {device}")
+        else:
+            _LOGGER.error(f"Device with ID {device} not found")
+
         if entity:
             node_id = entity.attributes.get("node_id")
             if node_id is not None:
@@ -27,19 +45,33 @@ async def setup_services(hass: HomeAssistant, websocket, send_command):
                         await send_command(websocket, 'poweraction', {'nodeids': [node_id], 'actiontype': 3})
                     case "sleep":
                         await send_command(websocket, 'poweraction', {'nodeids': [node_id], 'actiontype': 4})
-                _LOGGER.debug(f"Turning device ({name}) into mode '{mode}' with node_id: {node_id}")
 
     async def handle_notify(call: ServiceCall):
-        entity_id = call.data.get("entity_id")
+        device = call.data.get("device")
         message = call.data.get("message")
         title = call.data.get("title", "HomeAssistant")
 
-        entity = hass.states.get(entity_id)
+        device_registry = async_get_device(hass)
+        entity_registry = async_get_entity(hass)
+
+        # Lade Device
+        device_entry = device_registry.async_get(device)
+
+        if device_entry:
+            entities = async_entries_for_device(entity_registry, device_entry.id)
+            
+            if entities:
+                entity = hass.states.get(entities[0].entity_id)
+            else:
+                _LOGGER.error(f"No entities found for device {device}")
+        else:
+            _LOGGER.error(f"Device with ID {device} not found")
+
+
         if entity:
             node_id = entity.attributes.get("node_id")
             if node_id is not None:
                 name = entity.attributes.get("name")
-                _LOGGER.debug(f"Sending Notification to ({name}) with node_id: {node_id}")
                 await send_command(websocket, "msg", {
                     'msg': message,
                     'title': title,
@@ -48,12 +80,12 @@ async def setup_services(hass: HomeAssistant, websocket, send_command):
                 })
 
     hass.services.async_register(DOMAIN, "power", handle_power, schema=vol.Schema({
-        vol.Required("entity_id"): cv.entity_id,
+        vol.Required("device"): cv.string,
         vol.Required("mode"): cv.string
     }))
 
     hass.services.async_register(DOMAIN, "notify", handle_notify, schema=vol.Schema({
-        vol.Required("entity_id"): cv.entity_id,
+        vol.Required("device"): cv.string,
         vol.Required("message"): cv.string,
         vol.Required('title', default='HomeAssistant'): cv.string
     }))
